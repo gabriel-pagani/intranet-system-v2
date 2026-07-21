aa2752d241e21842ba27503d227cbbcffca08020
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-07-21 14:00:26 -0300

    Add DashboardsAdmin class and update UsersAdmin to include dashboards field

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import Group
-from .models import Users, GroupProxy, Setores, Ramais
+from .models import Users, GroupProxy, Setores, Ramais, Dashboards, GroupDashboards
 
 
 # Users Admin
@@ -10,7 +10,7 @@ class UsersAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'last_login', 'date_joined', 'is_staff', 'is_superuser', 'is_active',)
     search_fields = ('username', 'email', 'first_name', 'last_name', 'observations',)
     list_filter = ('is_active', 'is_staff', 'is_superuser', 'groups',)
-    filter_horizontal = ('groups', 'user_permissions',)
+    filter_horizontal = ('groups', 'user_permissions', 'dashboards',)
     model = Users
     ordering = ('username',)
     fieldsets = (
@@ -21,7 +21,7 @@ class UsersAdmin(UserAdmin):
             'fields': ('first_name', 'last_name', 'email',)
         }),
         ('Permissões', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'dashboards',)
         }),
         ('Datas importantes', {
             'fields': ('last_login', 'date_joined',)
@@ -56,3 +56,13 @@ class RamaisAdmin(admin.ModelAdmin):
     search_fields = ('name', 'user__username', 'user__first_name', 'user__last_name', 'phone', 'sector__name', 'machine',)
     list_filter = ('sector',)
     autocomplete_fields = ('sector', 'user')
+
+
+# Dashboards Admin
+@admin.register(Dashboards)
+class DashboardsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sector', 'status')
+    search_fields = ('title', 'sector__name')
+    filter_horizontal = ('fav_by',)
+    list_filter = ('status', 'sector',)
+    autocomplete_fields = ('sector',)
