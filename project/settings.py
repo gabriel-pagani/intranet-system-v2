adcbf298b1dc47fd0c2c1cab70c84c14921811c3
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-07-22 10:11:36 -0300

    Add LDAP authentication configuration to settings and update environment variables

--- project/settings.py
+++ project/settings.py
@@ -107,3 +107,38 @@ AUTH_USER_MODEL = 'app.Users'
 METABASE_SITE_URL = os.getenv('METABASE_SITE_URL')
 
 METABASE_SECRET_KEY = os.getenv('METABASE_SECRET_KEY')
+
+# Auth LDAP
+from django_auth_ldap.config import LDAPSearch, ActiveDirectoryGroupType
+from ldap import SCOPE_SUBTREE
+
+AUTH_LDAP_SERVER_URI = os.getenv('AUTH_LDAP_SERVER_URI')
+
+AUTH_LDAP_BIND_DN = os.getenv('AUTH_LDAP_BIND_DN')
+
+AUTH_LDAP_BIND_PASSWORD = os.getenv('AUTH_LDAP_BIND_PASSWORD')
+
+AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
+
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    os.getenv('AUTH_LDAP_USER_SEARCH'),
+    SCOPE_SUBTREE,
+    "(sAMAccountName=%(user)s)"
+)
+
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+    "username": "uid",
+}
+
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+
+AUTH_LDAP_MIRROR_GROUPS = True
+
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    os.getenv('AUTH_LDAP_GROUP_SEARCH'),
+    SCOPE_SUBTREE,
+    "(objectClass=group)"
+)
