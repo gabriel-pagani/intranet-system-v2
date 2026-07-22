89cfb5954eddb1645b79dbba42a0bd912cf07d42
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-07-22 08:22:18 -0300

    Update admin site header to reflect correct title in Portuguese

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Template"
+admin.site.site_header = "Administração"
 admin.site.site_title = "Sistema"
 admin.site.index_title = "Painel de Controle"
 
